7c116c911e2c8129c111d7923166799471d82ae8
GeorgeChousiadas <chousiadasgeorge@gmail.com>
2020-08-26 17:00:07 +0300

    MyCommit

--- src/main/java/gr/codehub/ecomerce/Factorial.java
+++ src/main/java/gr/codehub/ecomerce/Factorial.java
@@ -7,12 +7,21 @@ public class Factorial{
     }
 
     // method to compute the factorial of a number
-    public int computeFactorial(int x){
+    public long computeFactorial(int x){
 
-        int i,fact=1;
+        /*int i,fact=1;
         for(i=1;i<=x;i++){
             fact=fact*i;
         }
         return fact; //return the result
+
+        */
+
+        long factorial = 1;
+        for(int i = 1; i <= x; ++i) {
+            // factorial = factorial * i;
+            factorial *= i;
+        }
+        return factorial;
     }
 }
